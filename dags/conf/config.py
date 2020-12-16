c1eb9d0ce7025f2a460cb4183ef30f5d22971aaa
jnv88 <jacob.neal.vann@gmail.com>
2020-12-16 14:05:19 +0000

    changed config file

--- dags/conf/config.py
+++ dags/conf/config.py
@@ -3,23 +3,30 @@ from datetime import date
 import os.path
 
 today = str(date.today()).replace('-', '')
-file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.ini')
+config_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.cfg')
+
 config = ConfigParser()
-config.read(file)
+config.read(config_file)
 
 # covid api
-covid_api_url = config['covid-api']['url']
-covid_api_headers = {'x-rapidapi-key': config['covid-api']['key'], 'x-rapidapi-host': config['covid-api']['host']}
-covid_api_conn_id = config['covid-api']['conn']
-
-# s3
-covid_data_bucket = config['s3']['bucket']
-s3_key = '{0}_covid_data.csv'.format(today)
-s3_conn_id = config['s3']['conn']
-
-# email
-email = config['other']['email']
+COVID_API_URL = config.get('COVID-API', 'URL')
+COVID_API_KEY = config.get('COVID-API', 'KEY')
+COVID_API_KEY_LABEL = config.get('COVID-API', 'KEY_LABEL')
+COVID_API_HOST = config.get('COVID-API', 'HOST')
+COVID_API_HOST_LABEL = config.get('COVID-API', 'HOST_LABEL')
+COVID_API_CONN_ID = config.get('COVID-API', 'CONN')
+COVID_API_HEADERS = {COVID_API_KEY_LABEL: COVID_API_KEY, COVID_API_HOST_LABEL: COVID_API_HOST}
 
 # files
-file_name = 'dags/files/{0}_covid_data.csv'.format(today)
+COVID_DATA = 'dags/files/{0}_{1}'.format(today, config.get('FILE', 'COVID_DATA'))
+
+# s3
+COVID_DATA_BUCKET = config.get('S3', 'BUCKET')
+S3_KEY = '{0}_{1}'.format(today, config.get('FILE', 'COVID_DATA'))
+S3_CONN_ID = config.get('S3', 'CONN')
+
+# email
+AIRFLOW_EMAIL = config.get('OTHER', 'EMAIL')
+
+
 
--- dags/covid_data_pipeline.py
+++ dags/covid_data_pipeline.py
@@ -14,17 +14,17 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'email': cfg.email,
+    'email': cfg.AIRFLOW_EMAIL,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
 
 
-def download_data(url, headers, file_name):
+def download_data(url, headers, file):
     response = requests.request("GET", url, headers=headers)
     covid_data_json = response.json()['response']
     covid_data_df = pd.json_normalize(covid_data_json)
-    covid_data_df.to_csv(file_name, index=False)
+    covid_data_df.to_csv(file, index=False)
 
 
 def upload_to_s3(file_name, key, bucket_name, conn_id):
@@ -41,10 +41,10 @@ with DAG(
     is_covid_data_available = HttpSensor(
         task_id='is_covid_data_available',
         method='GET',
-        http_conn_id=cfg.covid_api_conn_id,
+        http_conn_id=cfg.COVID_API_CONN_ID,
         endpoint='latest',
         response_check=lambda response: 'response' in response.text,
-        headers=cfg.covid_api_headers,
+        headers=cfg.COVID_API_HEADERS,
         poke_interval=1,
         timeout=5
     )
@@ -52,13 +52,13 @@ with DAG(
     download_covid_data = PythonOperator(
         task_id='download_covid_data',
         python_callable=download_data,
-        op_args=[cfg.covid_api_url, cfg.covid_api_headers, cfg.file_name]
+        op_args=[cfg.COVID_API_URL, cfg.COVID_API_HEADERS, cfg.COVID_DATA]
     )
 
     upload_data_to_s3 = PythonOperator(
         task_id='upload_data_to_S3',
         python_callable=upload_to_s3,
-        op_args=[cfg.file_name, cfg.s3_key, cfg.covid_data_bucket, cfg.s3_conn_id]
+        op_args=[cfg.COVID_DATA, cfg.S3_KEY, cfg.COVID_DATA_BUCKET, cfg.S3_CONN_ID]
     )
 
 is_covid_data_available >> download_covid_data >> upload_data_to_s3
